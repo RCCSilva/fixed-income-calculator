5989e6ac777bddbc558772209df0fbe70326d36b
rafae.silva <rafael.silva@rappi.com>
2020-09-26 23:44:16 -0300

    Updates

---
+++ precache-manifest.d48792f9ad70076dd96985a366e3f2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991ceda227f30f7b2636ea29bea41e6f",
+    "url": "/renda-fixa/index.html"
+  },
+  {
+    "revision": "cadac45a401c5c7f9d7c",
+    "url": "/renda-fixa/static/css/main.241cbd23.chunk.css"
+  },
+  {
+    "revision": "dcdee38a1ec587e80480",
+    "url": "/renda-fixa/static/js/2.fd55f9b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/renda-fixa/static/js/2.fd55f9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadac45a401c5c7f9d7c",
+    "url": "/renda-fixa/static/js/main.93875e9a.chunk.js"
+  },
+  {
+    "revision": "c06ad2d34b5ccf3b5609",
+    "url": "/renda-fixa/static/js/runtime-main.d111b801.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7747630b05e016b12c4a4cccb4c125f.js"
+  "/renda-fixa/precache-manifest.d48792f9ad70076dd96985a366e3f2f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/renda-fixa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
